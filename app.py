ff217ded0896e101c2844f4f4fda5d6dfaa7c02c
Philip Kang <philip.kang@appian.com>
2023-06-15 13:30:53 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ PROVIDER_MAP = {
     'flanxl': 'Flan XL'
 }
 
+region = os.environ["AWS_REGION"]
+kendra_index_id = os.environ["KENDRA_INDEX_ID"]
+endpoint_name = os.environ["FLAN_XL_ENDPOINT"]
+
 # Check if the user ID is already stored in the session state
 if 'user_id' in st.session_state:
     user_id = st.session_state['user_id']
